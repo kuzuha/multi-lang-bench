3973b9bdef8042f33aa7c3aa946dd0c0c5569004
esehara <esehara@gmail.com>
2013-12-23 17:12:13 +0900

    fix: multi import, blank fix

--- python/bench.py
+++ python/bench.py
@@ -2,6 +2,12 @@ import sys
 import time
 
 
+def easy_import(target):
+    return __import__(
+        'script.' + target,
+        globals(), locals(), ['main'], -1)
+
+
 def execute(target, count):
     start = time.time()
     for do_run in xrange(count):
@@ -11,14 +17,14 @@ def execute(target, count):
 
 def main():
     count = int(sys.argv[1])
-    use_lib = sys.argv[2]
-    target = __import__(
-        'script.' + use_lib,
-        globals(), locals(), ['main'], -1)
-    time = execute(target, count)
-    print "# %s" % use_lib
-    print "## %f" % time
+    blank = easy_import("blank")
+    fix_time = execute(blank, count)
 
+    for target_name in sys.argv[2:]:
+        target = easy_import(target_name)
+        time = execute(target, count)
+        print "# %s" % target_name
+        print "## %f" % (time - fix_time)
 
 if __name__ == "__main__":
     main()
